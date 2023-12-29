760bac3d93cb7306fd70ac50d3fb66329ca2ba11
RamaRehawe <ramarehawe51@gmail.com>
2023-12-29 19:19:56 +0300

    Last change

--- Form1.cs
+++ Form1.cs
@@ -20,14 +20,12 @@ namespace Server_Network_App
         // constants
         private const string CRLF = "\r\n";
 
-
         // fields
         private List<TcpClient> _client_list;
         private TcpListener _listener;
         private int _client_count;
         private bool _keep_going;
         private int _port = 5000;
-
         private UdpClient _udpClient;
         private Thread _udpThread;
         private int _udpPort = 54321;
@@ -44,10 +42,7 @@ namespace Server_Network_App
             _statusTextBox.Text = string.Empty;
         }
 
-
-
         #region Event Handlers
-
         private void StartServerButtonHandler(object sender, EventArgs e)
         {
             _client_count = 0;
@@ -59,19 +54,16 @@ namespace Server_Network_App
                     _port = 5000;
                     _statusTextBox.Text += CRLF + "You entered an invalid port value. Using port " + _port;
                 }
-
                 // Close existing UDP client
                 if (_udpClient != null)
                 {
                     _udpClient.Close();
                 }
-
                 // Start TCP server
                 Thread tcpThread = new Thread(ListenForIncomingConnections);
                 tcpThread.Name = "Server Listener Thread";
                 tcpThread.IsBackground = true;
                 tcpThread.Start();
-
                 // Start UDP server
                 _udpClient = new UdpClient(_udpPort);
                 _udpThread = new Thread(ListenUDP);
@@ -93,23 +85,17 @@ namespace Server_Network_App
             _keep_going = false;
             _statusTextBox.Text = string.Empty;
             _statusTextBox.Text = "Shuting down server, disconnectiong all clients...";
-            //_client_count = 0;
-
             try
             {
                 foreach(TcpClient client in _client_list)
                 {
                     client.Close();
-                    
                 }
                 _connectedClientsTextBox.InvokeEx(cctb => cctb.Text = _client_count.ToString());
                 
                 _client_list.Clear();
                 _listener.Stop();
                 _udpClient.Close();
-                //_udpThread.Join(); // Wait for UDP thread to finish
-
-
             }
             catch(Exception ex)
             {
@@ -121,23 +107,18 @@ namespace Server_Network_App
             _sendCommandButton.Enabled = false;
 
         } // End of StopServerButtonHandler
-
         private void SendCommandButtonHandler(object sender, EventArgs e)
         {
             try
             {
-
                 foreach(TcpClient client in _client_list)
                 {
                     StreamWriter writer = new StreamWriter(client.GetStream());
                     writer.WriteLine(_clientCommandTextBox.Text);
                     writer.Flush();
-                    
                 }
                 //_statusTextBox.Text += CRLF + "Command Sent from Server: " + _clientCommandTextBox.Text;
                 _clientCommandTextBox.Text = string.Empty;
-
-
             }catch (Exception ex)
             {
                 _statusTextBox.Text += CRLF + "Problem sending command to clients";
@@ -145,7 +126,6 @@ namespace Server_Network_App
             }
         } // End of SendCommandButtonHandler
         #endregion Event Handlers
-
         private void ListenForIncomingConnections()
         {
             try
@@ -154,7 +134,7 @@ namespace Server_Network_App
                 _listener = new TcpListener(IPAddress.Any, _port);
                 _listener.Start();
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Tcp Server started. Listening on port: " + _port);
-                while(_keep_going)
+                while (_keep_going)
                 {
                     _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Waiting for incoming client connections...");
                     TcpClient client = _listener.AcceptTcpClient(); // blocks here until client connects
@@ -163,24 +143,19 @@ namespace Server_Network_App
                     t.IsBackground = true;
                     t.Start(client);
                 }
-
             }catch (Exception ex)
             {
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + ex.ToString());
             }
-
             _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Exiting listener thred...");
             _statusTextBox.InvokeEx(stb => stb.Text = string.Empty);
-
         } // End ListenForIncomingConnections method
-
         private void ProcessClientRequests(object o)
         {
             TcpClient client = (TcpClient)o;
             _client_list.Add(client);
             _client_count += 1;
             _connectedClientsTextBox.InvokeEx(cctb => cctb.Text =  _client_count.ToString());
-
             string input = string.Empty;
             try
             {
@@ -197,11 +172,9 @@ namespace Server_Network_App
                                 stb.Text += CRLF + "From client " + client.GetHashCode() + ": " + input);
                                 writer.WriteLine("Server recieved: " + input);
                                 writer.Flush();
-
                                 break;
                             }
                     }
-
                     SendToUDPServer(input);
                 }
             }catch(SocketException se)
@@ -219,9 +192,7 @@ namespace Server_Network_App
             _connectedClientsTextBox.InvokeEx(cctb => cctb.Text = _client_count.ToString());
             _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Finished processing client requests for client: " + client.GetHashCode());
 
-
         } // End ProcessClientRequests method
-
         private void SendToUDPServer(string message)
         {
             try
@@ -231,24 +202,17 @@ namespace Server_Network_App
 
                 // Send the message using UDP
                 _udpClient.Send(data, data.Length, new IPEndPoint(IPAddress.Broadcast, _udpPort));
-
-                // Additional status or logging if needed
-                //_statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Message sent to UDP server: " + message);
             }
             catch (Exception ex)
             {
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Problem sending message to UDP server: " + ex.ToString());
             }
         } // End of SendToUDPServer method
-
-
-
         private void ListenUDP()
         {
             try
             {
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "UDP Server started. Listening on port: " + _udpPort);
-
                 while (_keep_going)
                 {
                     IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
@@ -257,9 +221,6 @@ namespace Server_Network_App
 
                     // Broadcast the UDP message to all clients
                     BroadcastToClients(receivedMessage);
-
-                    // Additional processing if needed
-                    //_statusTextBox.InvokeEx(stb => stb.Text += CRLF + "UDP received from client: " + receivedMessage);
                 }
             }
             catch (Exception ex)
@@ -282,7 +243,6 @@ namespace Server_Network_App
                     writer.WriteLine(message);
                     writer.Flush();
                 }
-
                 // Additional status or logging if needed
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Message broadcast to clients: " + message);
             }
@@ -291,7 +251,5 @@ namespace Server_Network_App
                 _statusTextBox.InvokeEx(stb => stb.Text += CRLF + "Problem broadcasting message to clients: " + ex.ToString());
             }
         } // End of BroadcastToClients method
-
-
     }
 }
